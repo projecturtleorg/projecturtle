7c2c9697a32dce9b6c2f9f1eee2847f203c5cc2d
projecturtleorg <96139178+projecturtleorg@users.noreply.github.com>
2021-12-16 07:58:58 +0400

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+       /* Set the width of the side navigation to 250px */
+       function openNav() {
+           document.getElementById("mySidenav").style.width = "250px";
+           document.getElementById("navmain").style.display = 'none';
+       }
+
+       /* Set the width of the side navigation to 0 */
+       function closeNav() {
+           document.getElementById("mySidenav").style.width = "0";
+           document.getElementById("navmain").style.display = 'flex';
+       }
